b3aaa190d584f853da89c6c65a8bc35d5378254b
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-04-03 17:14:23 +0530

    use nvim-utils

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -310,68 +310,16 @@ return {
             vim.g.tabby_inline_completion_trigger = "auto"
         end,
     },
-    -- {
-    --   "yetone/avante.nvim",
-    --   event = "VeryLazy",
-    --   lazy = false,
-    --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    --   opts = {
-    --     provider = "copilot",
-    --   },
-    --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    --   build = "make",
-    --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    --   dependencies = {
-    --     "stevearc/dressing.nvim",
-    --     "nvim-lua/plenary.nvim",
-    --     "MunifTanjim/nui.nvim",
-    --     --- The below dependencies are optional,
-    --     "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    --     "zbirenbaum/copilot.lua", -- for providers='copilot'
-    --     {
-    --       -- support for image pasting
-    --       "HakonHarnes/img-clip.nvim",
-    --       event = "VeryLazy",
-    --       opts = {
-    --         -- recommended settings
-    --         default = {
-    --           embed_image_as_base64 = false,
-    --           prompt_for_file_name = false,
-    --           drag_and_drop = {
-    --             insert_mode = true,
-    --           },
-    --           -- required for Windows users
-    --           use_absolute_path = true,
-    --         },
-    --       },
-    --     },
-    --     {
-    --       -- Make sure to set this up properly if you have lazy=true
-    --       'MeanderingProgrammer/render-markdown.nvim',
-    --       opts = {
-    --         file_types = { "markdown", "Avante" },
-    --       },
-    --       ft = { "markdown", "Avante" },
-    --     },
-    --   },
-    -- },
-
-    -- filetype plugin on
-    --
-    -- " Section for plugins managed by vim-plug
-    -- call plug#begin('~/.vim/plugged')
-    --
-    -- " Tabby plugin
-    -- Plug 'TabbyML/vim-tabby'
-    -- " Add config here. Example config:
-    -- let g:tabby_keybinding_accept = '<Tab>'
-    --
-    -- " Configure node >= 18
-    -- let g:tabby_node_binary = '/prod/tools/infra/nodejs/node20/node/bin/node'
-    --
-    -- call plug#end()
+    {
+        "krshrimali/nvim-utils",
+        config = function()
+            require("tgkrsutil").setup({
+            enable_test_runner = true,
+            test_runner = function(file, func)
+                return string.format("pytest %s -k %s", file, func)
+            end,
+            })
+        end,
+        event = "VeryLazy",
+    }
 }
