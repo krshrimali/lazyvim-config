0f1b2fdf827f224c0017525930bdb978bf588e64
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-04-03 17:15:43 +0530

    Cleanup in coc keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,11 +40,11 @@ function _G.check_back_space()
   return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
 end
 --
--- -- Use Tab for trigger completion with characters ahead and navigate
--- -- NOTE: There's always a completion item selected by default, you may want to enable
--- -- no select by setting `"suggest.noselect": true` in your configuration file
--- -- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
--- -- other plugins before putting this into your config
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
@@ -54,7 +54,7 @@ keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
 -- Use <c-j> to trigger snippets
-keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
+-- keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
 -- Use <c-space> to trigger completion
 keyset("i", "<c-space>", "coc#refresh()", { silent = true, expr = true })
 
@@ -157,9 +157,9 @@ keyset("v", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', o
 
 -- Use CTRL-S for selections ranges
 -- Requires 'textDocument/selectionRange' support of language server
-keyset("n", "<C-s>", "<Plug>(coc-range-select)", { silent = true })
-keyset("x", "<C-s>", "<Plug>(coc-range-select)", { silent = true })
-
+-- keyset("n", "<C-s>", "<Plug>(coc-range-select)", { silent = true })
+-- keyset("x", "<C-s>", "<Plug>(coc-range-select)", { silent = true })
+--
 -- Add `:Format` command to format current buffer
 vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
 
@@ -183,17 +183,17 @@ local opts = { silent = true, nowait = true }
 -- Manage extensions
 -- keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
 -- Show commands
-keyset("n", "<space>c", ":<C-u>CocList commands<cr>", opts)
+keyset("n", "<space>cc", ":<C-u>CocList commands<cr>", opts)
 -- Find symbol of current document
-keyset("n", "<space>o", ":<C-u>CocList outline<cr>", opts)
+keyset("n", "<space>co", ":<C-u>CocList outline<cr>", opts)
 -- Search workspace symbols
-keyset("n", "<space>s", ":<C-u>CocList -I symbols<cr>", opts)
+keyset("n", "<space>cs", ":<C-u>CocList -I symbols<cr>", opts)
 -- Do default action for next item
 -- keyset("n", "<space>j", ":<C-u>CocNext<cr>", opts)
 -- -- Do default action for previous item
 -- keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 -- -- Resume latest coc list
-keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
+keyset("n", "<space>cp", ":<C-u>CocListResume<cr>", opts)
 
 --
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
