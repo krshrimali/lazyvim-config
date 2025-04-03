e1b1ff4205d4f355cf2e4cbe99dc479b60882587
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-04-03 17:16:02 +0530

    Cleanup in options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,6 +26,7 @@ vim.opt.clipboard:append('unnamedplus')
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.kaz_transparency = false
+-- vim.g.ai_cmp = false
 -- vim.g.lazyvim_python_lsp = "basedpyright"
 
 vim.opt.cursorlineopt = vim.g.kaz_transparency and "number" or "both"
